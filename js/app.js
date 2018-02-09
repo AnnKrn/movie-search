c02887feef5cb671f2a8a8dec0156c6c7807d941
AnnKrn <anakrn.carreon@gmail.com>
2018-02-08 18:18:35 -0600

    datos dinamicos

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ var $input
 var API_TRAILER = "https://api.themoviedb.org/3/movie/"
 var videos = "/videos?"
 
+var API_IMAGE = "https://image.tmdb.org/t/p/w500"
+// var image = "/images?"
+
 const request = superagent
 
 function getJson() {
@@ -20,22 +23,44 @@ function getJson() {
 	// console.log(url)
 }
 
-function movieAsked() {
+function movieAsked(e) {
+	e.preventDefault()
 	var url = API_ENDPOINT + api_key + lang + query + $input.val()
 	//console.log(url)
 	request
 	.get(url)
 	.then(function(response) {
 		const responseObject = response.body
-		// console.log(responseObject)
+		console.log(responseObject)
 		const movieTitle = responseObject.results
-		console.log(movieTitle)
+		// console.log(movieTitle)
 		const movieTriler = movieTitle[0]
-		// console.log(responseObject)
+		// console.log(movieTriler)
 		const id = movieTriler.id
+		const posterPath = movieTriler.poster_path
+		// console.log(posterPath)
+
+		const voteAverage = movieTriler.vote_average
+		const release = movieTriler.release_date
+		
+		// console.log(voteAverage)
+		// console.log(movieTriler.id)		
+		// Sinopsis
 		const overview = movieTriler.overview
+		// console.log(overview)	
 		const name = movieTriler.original_title
+		// console.log(name)		titulo
 		const video = movieTriler
+		// console.log(video)	
+
+		var urlImages = API_IMAGE + posterPath
+		// console.log(urlImages)
+		$('#movie_image').attr('src',urlImages)
+		$('.movie_title').html(name)
+		$('#sinopsis').html(overview)
+		$('#rating').html(voteAverage)
+		$('#premier').html(release)
+
 		// document.write(name + ":" + overview + id)
 		$("#modal1").click(showTrailer(id))
 	})
@@ -52,7 +77,7 @@ function showTrailer(id) {
 	.get(urlTrailer)
 	.then(function(response) {
 		const responseObject = response.body
-		// console.log(responseObject)
+		console.log(responseObject)
 		const movieTrailer = responseObject.results[0]
 		const keyTrailer = movieTrailer.key
 		// console.log(keyTrailer)
